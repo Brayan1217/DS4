1d9e37851fe67d28a656b8df62c63e427b2c8fe1
Alexis <brayanmartinez1298@gmail.com>
2025-10-13 18:46:17 -0500

    laboratorio11

---
+++ Laboratorios/isma/isma/Parcial_ismael.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Parcial1
+{
+    static void Main(string[] args)
+    {
+        Parcial1 Parcial1 = new Parcial1();
+        Parcial1.panel();
+    }
+
+    public int[,] mat;
+
+    public void panel()
+    {
+
+        mat = new int[5, 5];
+        int suma = 0;
+        Random r = new Random(); 
+
+        for (int f = 0; f < 5; f++)
+        {
+            for (int c = 0; c < 5; c++)
+            {
+                    // esto es para que genere los valores en figura de cruz
+                if (f == 2 || c == 2)
+                {
+                    int N = r.Next(1, 100);
+
+                    if (N % 2 == 0)
+                    {
+                        mat[f, c] = 0;
+                    }
+                    else
+                    {
+                        mat[f, c] = N;
+                        suma += mat[f, c];
+                    }
+                }
+             
+
+                Console.Write(mat[f, c] + "      ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("La suma de los impares es: " + suma);
+    }
+}
---
+++ Laboratorios/parcial1/parcial1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Matriz maz = new Matriz();
+        maz.ingresarValores();
+        maz.imprimir();
+
+        int suma = maz.Suma();
+        Console.WriteLine("La suma de las diagonales es: " + suma);
+    }
+}
