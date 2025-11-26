ebcb6fcc91e4601cd1b62fc0d7f1f11e444b4918
Brayan A <brayanmartinez1298@gmail.com>
2025-11-25 21:58:27 -0500

    lab 19

---
+++ Laboratorios/laboratorio19/laboratorio192/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace laboratorio192.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string API_BASE_URL = "https://localhost:44302/Help";
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
